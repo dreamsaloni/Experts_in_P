7fcced7a6edde9b0cd7d6b81378a0aa9652e4b8d
dreamsaloni <103926119+dreamsaloni@users.noreply.github.com>
2022-11-09 21:08:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,96 @@
+function result()
+{
+    var score = 0;
+    if (document.getElementById('correct1').checked) 
+    {
+        score++;
+    }
+    if (document.getElementById('correct2').checked) 
+    {
+        score++;
+    }
+    if (document.getElementById('correct3').checked) 
+    {
+        score++;
+    }
+    if (document.getElementById('correct4').checked) 
+    {
+        score++;
+    }
+    if (document.getElementById('correct5').checked) 
+    {
+        score++;
+    }
+    if (document.getElementById('correct6').checked) 
+    {
+        score++;
+    }
+    if (document.getElementById('correct7').checked) 
+    {
+        score++;
+    }
+    if (document.getElementById('correct8').checked) 
+    {
+        score++;
+    }
+    if (document.getElementById('correct9').checked) 
+    {
+        score++;
+    }
+    if (document.getElementById('correct10').checked) 
+    {
+        score++;
+    }
+    if (document.getElementById('correct11').checked) 
+    {
+        score++;
+    }
+    if (document.getElementById('correct12').checked) 
+    {
+        score++;
+    }
+    if (document.getElementById('correct13').checked) 
+    {
+        score++;
+    }
+    if (document.getElementById('correct14').checked) 
+    {
+        score++;
+    }
+    if (document.getElementById('correct15').checked) 
+    {
+        score++;
+    }
+    if (document.getElementById('correct16').checked) 
+    {
+        score++;
+    }
+    if (document.getElementById('correct17').checked) 
+    {
+        score++;
+    }
+    if (document.getElementById('correct18').checked) 
+    {
+        score++;
+    }
+    if (document.getElementById('correct19').checked) 
+    {
+        score++;
+    }
+    if (document.getElementById('correct20').checked) 
+    {
+        score++;
+    }
+    if (document.getElementById('correct21').checked) 
+    {
+        score++;
+    }
+    
+    
+    alert("Congratulations!! Your score is : " +score );
+    
+}
+
+        
+
+    
